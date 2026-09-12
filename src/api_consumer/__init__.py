67215d1f4326c6b9c7193a78e363a201cb2bb415
Arash Mohammadi <arashmohammadi73@gmail.com>
2026-09-12 09:52:19 +0330

    Add public API surface and command-line interface

--- src/api_consumer/__init__.py
+++ src/api_consumer/__init__.py
@@ -1,1 +1,7 @@
 """Client that creates and deletes a group across all nodes of a cluster."""
+
+from api_consumer.client import ClusterClient, ClusterOperationError, OperationResult
+from api_consumer.node_api import NodeState
+from api_consumer.retry import RetryPolicy
+
+__all__ = ["ClusterClient", "ClusterOperationError", "NodeState", "OperationResult", "RetryPolicy"]
